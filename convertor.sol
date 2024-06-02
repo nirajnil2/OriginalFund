04630de6de4aacf4433ab0abc9edd1a6bd21c0a1
Niraj Kumar <118387484+nirajnil2@users.noreply.github.com>
2024-06-02 06:45:42 +0530

    Create convertor.sol

---
+++ convertor.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+contract fund{
+    uint256 myweivalue =1000000000000000000;
+    function weicovertor(uint256 value) public {
+        myweivalue *= value;
+       
+
+    }
+    function gweicovertor(uint256 value) public {
+        
+
+
+    }
+
+}
+
